c25356804e3e0534d747ff08c4ea322b73b12753
Helder Barboza <29435727+helderbarboza@users.noreply.github.com>
2024-11-01 02:46:53 -0400

    add `mode-watcher`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+  import { ModeWatcher } from 'mode-watcher'
+
   import '../app.css'
 
   const { children } = $props()
 </script>
+
+<ModeWatcher />
 
 <main class="container">
   {@render children()}
