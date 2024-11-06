603a90242ae120604153156b6f2eba6c19cea6c0
Helder Barboza <29435727+helderbarboza@users.noreply.github.com>
2024-11-06 04:13:23 -0400

    format

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
-	import type { HTMLInputAttributes } from "svelte/elements";
-	import type { WithElementRef } from "bits-ui";
-	import { cn } from "$lib/utils.js";
+  import type { WithElementRef } from 'bits-ui'
+  import type { HTMLInputAttributes } from 'svelte/elements'
+  import { cn } from '$lib/utils.js'
 
-	let {
-		ref = $bindable(null),
-		value = $bindable(),
-		class: className,
-		...restProps
-	}: WithElementRef<HTMLInputAttributes> = $props();
+  let {
+    ref = $bindable(null),
+    value = $bindable(),
+    class: className,
+    ...restProps
+  }: WithElementRef<HTMLInputAttributes> = $props()
 </script>
 
 <input
-	bind:this={ref}
-	class={cn(
-		"border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
-		className
-	)}
-	bind:value
-	{...restProps}
+  bind:this={ref}
+  class={cn(
+    `
+      flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm
+      transition-colors disabled:cursor-not-allowed disabled:opacity-50 file:border-0
+      file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none
+      focus-visible:ring-1 focus-visible:ring-ring placeholder:text-muted-foreground
+    `,
+    className,
+  )}
+  bind:value
+  {...restProps}
 />
